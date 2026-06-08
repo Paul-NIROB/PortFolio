17826fb5885788510ba914c88548fbe3889525ff
nirob <nirobpaulgetit@gmail.com>
2026-06-08 14:16:28 +0530

    Update profile image with actual photo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import profileImg from '../assets/hero.png';
+import profileImg from '../assets/profile.jpeg';
 
 const About = () => {
   return (
